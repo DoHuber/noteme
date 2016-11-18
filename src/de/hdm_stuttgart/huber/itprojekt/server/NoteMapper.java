12da44102728804b8e0e9682800d299b6d1bb9dc
Corsin Behr <c.behr95@googlemail.com>
2016-11-18 18:09:14 +0100

    NoteMapper+ leere Methoden

--- src/de/hdm_stuttgart/huber/itprojekt/server/NoteMapper.java
+++ src/de/hdm_stuttgart/huber/itprojekt/server/NoteMapper.java
@@ -1,9 +1,62 @@
 package de.hdm_stuttgart.huber.itprojekt.server;
 
+import java.sql.*;
+import java.util.Vector;
+import de.hdm_stuttgart.huber.itprojekt.server.db.DBConnection;
+
 public class NoteMapper {
 	
 	
+	private static NoteMapper noteMapper = null;
+
+
+	
+	
+	
+	
+	public NoteMapper getNoteMapper(){
+		
+	}
+	
+	
+	
+	
+	public Note create(Note note){
+		
+	
+	  }
+	
+	
+	
+	
+	public Note findById(Note note){
+		
+	}
+	
+	
+	
+	public Note save(Note note){
+		
+	}
 	
 
+	
+	 public void delete(Note note) {
+		
+	 }
+	 
+	
+	
+	 protected static NoteMapper NoteMapper() {
+			if (noteMapper == null) {
+			   noteMapper = new NoteMapper();
+			    }
 
+			    return noteMapper;
+			  }	
+		
+	 
+	
+	
 }
+
