4cf14a3fa4f9d9f2b33751ee22c4958b7cc657d5
Lisa Küchler <lk090@hdm-stuttgart.de>
2016-11-20 13:31:00 +0100

    DB Connection

---
+++ src/de/hdm_stuttgart/huber/itprojekt/server/Test.java
@@ -0,0 +1,10 @@
+package de.hdm_stuttgart.huber.itprojekt.server;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/de/hdm_stuttgart/huber/itprojekt/server/db/DataMapper.java
@@ -0,0 +1,25 @@
+package de.hdm_stuttgart.huber.itprojekt.server.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataMapper {
+	   //2 Datenbank Zugangsdaten anlegen
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://207.223.162.139:3306/Notizbuch";
+	   static final String USER = "lisakuechler";
+	   static final String PASS = "itprojekt22";
+	   
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+	   Connection conn = null;
+	   
+	   //3. JDBC Treiber regestrieren
+	   Class.forName("com.mysql.jdbc.Driver");
+	   
+	   //4. Verbindung aufbauen
+	   conn = DriverManager.getConnection(DB_URL,USER,PASS);
+	   return conn;   
+	}
+
+}
