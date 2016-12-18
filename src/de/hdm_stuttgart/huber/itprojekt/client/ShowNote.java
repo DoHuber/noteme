cb90ea2ca4b1748f0391f9b93339d10e017c981b
Dominik Erdmann <de021@hdm-stuttgart.de>
2016-12-18 12:30:46 +0100

    Schreibfehler korrigiert

--- src/de/hdm_stuttgart/huber/itprojekt/client/ShowNote.java
+++ src/de/hdm_stuttgart/huber/itprojekt/client/ShowNote.java
@@ -31,7 +31,7 @@ public class ShowNote extends BasicView {
 	private HorizontalPanel vp = new HorizontalPanel();
 	private Button deleteBtn = new Button("Delete");
 	private Button editBtn = new Button("Update");
-	private Button releseBtn = new Button("Relese");
+	private Button releseBtn = new Button("Release");
 	
 	private RichTextArea noteArea = new RichTextArea();
 	private DateBox dueDateBox = new DateBox();
