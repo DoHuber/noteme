a7bf6b3c5caaf786ee26a27891f2742c39394bbb
NikitaNalivayko <nn021@hdm-stuttgart.de>
2016-12-23 11:58:21 +0100

    Änderung in ShowNote und ShowNotebook

--- src/de/hdm_stuttgart/huber/itprojekt/client/ShowNotebook.java
+++ src/de/hdm_stuttgart/huber/itprojekt/client/ShowNotebook.java
@@ -9,7 +9,9 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 
@@ -29,10 +31,13 @@ public class ShowNotebook extends BasicView{
 	private HorizontalPanel vp = new HorizontalPanel();
 	private Button deleteBtn = new Button("Delete");
 	private Button editBtn = new Button("Update");
-	private Button releseBtn = new Button("Relese");
+	private Button releseBtn = new Button("Release");
+	private Button createBtn = new Button ("Create Note");
 	EditorAsync editorVerwaltung = ClientsideSettings.getEditorVerwaltung();
 	NoteBook nb = null;
-	AllNotesCallback callback = new AllNotesCallback();
+	private TextBox title = new TextBox();
+	private TextBox subtitle = new TextBox();
+	//AllNotesCallback callback = new AllNotesCallback();
 	
 	
 	private Vector<Note> notes = new Vector<Note>();
@@ -58,39 +63,93 @@ public class ShowNotebook extends BasicView{
 
 		FlowPanel contentPanel = new FlowPanel();
 		vp.add(deleteBtn);
+		deleteBtn.addClickHandler(new DeleteClickHandler());
 		vp.add(editBtn);
 		vp.add(releseBtn);
-		editorVerwaltung.getAllNotes(callback);
-	    NoteTable nt = new NoteTable(notes);
-	    nt.addClickNote();
-	    RootPanel.get("main").clear();
+		vp.add(createBtn);
+		createBtn.addClickHandler(new CreateNoteClickHandler());
+		nb.getId();
+		title.setText(nb.getTitle());
+		subtitle.setText(nb.getSubtitle());
+		//editorVerwaltung.getAllNotes(callback);
+	   // NoteTable nt = new NoteTable(notes);
+	   // nt.addClickNote();
+	   
 	   
 		contentPanel.add(vp);
-		contentPanel.add(nt.start());
+		contentPanel.add(title);
+		contentPanel.add(subtitle);
+		//contentPanel.add(nt.start());
+		RootPanel.get("main").clear();
 		RootPanel.get("main").add(contentPanel);
 
 
 	}
-	private class AllNotesCallback implements AsyncCallback<Vector<Note>> {
-	    @Override
-	    public void onSuccess(Vector<Note> result) {
-	      addNotesToTable(result);
-	    }
+//	private class AllNotesCallback implements AsyncCallback<Vector<Note>> {
+//	    @Override
+//	    public void onSuccess(Vector<Note> result) {
+//	      addNotesToTable(result);
+//	    }
+//
+//	    @Override
+//	    public void onFailure(Throwable caught) {}
+//
+//		
+//	  }
+//	public void addNotesToTable(Vector<Note> result) {
+//	notes = result;
+//	NoteTable nt = new NoteTable(notes);
+//	nt.addClickNote();
+////	RootPanel.get("main").clear();
+////	RootPanel.get("main").add(nt.start());
+//}
+	private class DeleteClickHandler implements ClickHandler {
 
-	    @Override
-	    public void onFailure(Throwable caught) {}
-
+		@Override
+		public void onClick(ClickEvent event) {
+		if(	Window.confirm("Möchten Sie das Notizbuch "+ nb.getTitle()+ " wirklich löschen?")){
+			editorVerwaltung.deleteNoteBook(nb, new DeleteCallback());
+		}
+			
+			
+		}}
 		
-	  }
-	public void addNotesToTable(Vector<Note> result) {
-	notes = result;
-	NoteTable nt = new NoteTable(notes);
-	nt.addClickNote();
-	RootPanel.get("main").clear();
-	RootPanel.get("main").add(nt.start());
-}
+	
+	private class DeleteCallback implements AsyncCallback<Void>{
 
-}
+		@Override
+		public void onFailure(Throwable caught) {
+			caught.printStackTrace();
+			vp.add(new Label(caught.toString()));
+			
+		}
 
+		@Override
+		public void onSuccess(Void result) {
+//			MenuView navigation = new MenuView();
+//			RootPanel.get("menu").clear();
+//			RootPanel.get("menu").add(navigation);	
+//			
+//			ShowAllNotebooks san =  new ShowAllNotebooks();
+//			RootPanel.get("main").clear();
+//			RootPanel.get("main").add(san);
+			
+		}
+	}
+	private class CreateNoteClickHandler implements ClickHandler{
+
+		@Override
+		public void onClick(ClickEvent event) {
+			MenuView mView = new MenuView();
+			RootPanel.get("menu").clear();
+			RootPanel.get("menu").add(mView);
+			
+			CreateNote cN = new CreateNote();
+			RootPanel.get("main").clear();
+			RootPanel.get("main").add(cN);
+			
+		}
+}
+}
 
 
