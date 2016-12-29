44ec983bd2bf20087c475e4bd21932295d2bfd48
Dominik Huber <dominik.huber@interface-ag.de>
2016-12-29 16:56:09 +0100

    Bugfix: login nun von beiden Clients erreichbar

--- src/de/hdm_stuttgart/huber/itprojekt/client/NoteMeReport.java
+++ src/de/hdm_stuttgart/huber/itprojekt/client/NoteMeReport.java
@@ -14,9 +14,10 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm_stuttgart.huber.itprojekt.shared.ReportGeneratorAsync;
+import de.hdm_stuttgart.huber.itprojekt.shared.SharedServices;
+import de.hdm_stuttgart.huber.itprojekt.shared.SharedServicesAsync;
 import de.hdm_stuttgart.huber.itprojekt.shared.domainobjects.UserInfo;
 import de.hdm_stuttgart.huber.itprojekt.shared.report.AllNoteBooksOfAllUsersReport;
-import de.hdm_stuttgart.huber.itprojekt.shared.report.HTMLReportWriter;
 
 /**
  * Entry-Point-Klasse des Projekts <b>BankProjekt</b>.
@@ -28,9 +29,9 @@ public class NoteMeReport implements EntryPoint {
 	private UserInfo userInfo = null;
 	private VerticalPanel loginPanel = new VerticalPanel();
 	private Label loginLabel = new Label(
-	      "Please sign in to your Google Account to access the cool and nice application.");
+			"Please sign in to your Google Account to access the cool and nice application.");
 	private Anchor signInLink = new Anchor("Sign In");
-	
+
 	/**
 	 * Da diese Klasse die Implementierung des Interface <code>EntryPoint</code>
 	 * zusichert, benötigen wir eine Methode
@@ -40,69 +41,81 @@ public class NoteMeReport implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		
-		GWT.setUncaughtExceptionHandler(new UncaughtExceptionHandler(){
+
+		GWT.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
 
 			@Override
 			public void onUncaughtException(Throwable e) {
 				GWT.log(e.toString());
 			}
-			
+
 		});
+
+		SharedServicesAsync loginService = GWT.create(SharedServices.class);
+		loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<UserInfo>() {
+
+			public void onFailure(Throwable e) {
+
+			}
+
+			public void onSuccess(UserInfo result) {
+
+				userInfo = result;
+
+				if (userInfo.isLoggedIn()) {
+
+					loadMenu();
+
+				} else {
+					loadLogin();
+				}
+
+			}
+
+		});
+
+	}
+
+	private void loadMenu() {
 
 		if (reportGenerator == null) {
 			reportGenerator = ClientsideSettings.getReportGenerator();
 		}
-		
+
 		showReportButton = new Button("Irgendwas anziegen ahh!!");
-		
+
 		GWT.log(reportGenerator.toString());
 		GWT.log(showReportButton.toString());
-	
+
 		showReportButton.setStylePrimaryName("bankproject-menubutton");
 		showReportButton.addClickHandler(new ClickHandler() {
-			
+
 			@Override
 			public void onClick(ClickEvent event) {
-				
+
 				GWT.log("Onclick fired!");
 				reportGenerator.createAllNoteBooksOfAllUsersReport(new createAllNoteBooksOfAllUsersReportCallback());
-				
+
 			}
-			
-			
-			
-			public void onFailure(Throwable e) {
-				
-			}
-			
-			public void onSuccess(UserInfo result) {
-				
-				userInfo = result;
-				
-				if (userInfo.isLoggedIn()) {
-					
-					loadMenu();
-					
-				}
-				
-			}
-			
+
 		});
-		
+
 		RootPanel.get("menu").add(showReportButton);
-		
-		
-	}
-	
-private void loadMenu() {
-		
+
 		ShowReportDemo navigation = new ShowReportDemo();
 		ShowReportDemo.setLogOutUrl(userInfo.getLogoutUrl());
 		RootPanel.get("menu").add(navigation);
-	 	
+
 	}
-	
+
+	private void loadLogin() {
+
+		signInLink.setHref(userInfo.getLoginUrl());
+		loginPanel.add(loginLabel);
+		loginPanel.add(signInLink);
+		RootPanel.get("menu").add(loginPanel);
+
+	}
 
 	/**
 	 * Diese Nested Class wird als Callback für das Setzen des Bank-Objekts bei
@@ -119,8 +132,7 @@ private void loadMenu() {
 			 * Wenn ein Fehler auftritt, dann geben wir eine kurze Log Message
 			 * aus.
 			 */
-			ClientsideSettings.getLogger().severe(
-					"Setzen der Bank fehlgeschlagen!");
+			ClientsideSettings.getLogger().severe("Setzen der Bank fehlgeschlagen!");
 		}
 
 		@Override
@@ -140,9 +152,8 @@ private void loadMenu() {
 	 * @author rathke
 	 * @version 1.0
 	 */
-	class createAllNoteBooksOfAllUsersReportCallback implements
-			AsyncCallback<AllNoteBooksOfAllUsersReport> {
-		
+	class createAllNoteBooksOfAllUsersReportCallback implements AsyncCallback<AllNoteBooksOfAllUsersReport> {
+
 		@Override
 		public void onFailure(Throwable caught) {
 			/*
@@ -155,20 +166,19 @@ private void loadMenu() {
 
 		@Override
 		public void onSuccess(AllNoteBooksOfAllUsersReport report) {
-			
+
 			GWT.log("onSuccess reached!");
 			GWT.log(report.toString());
-			
+
 			if (report != null) {
-				
+
 				// HTMLReportWriter writer = new HTMLReportWriter();
 				// writer.process(report);
 				RootPanel.get("main").clear();
 				RootPanel.get("main").add(new HTML(report.getTitle() + report.getCreated().toString()));
 			}
 		}
-		
+
 	}
 
 }
-
